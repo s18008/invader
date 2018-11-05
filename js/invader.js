40cc7481e97ea9c3a83a1ed311ce78668af24807
s18008 <s18008@std.it-college.ac.jp>
2018-11-05 10:37:21 +0900

    playerのついか

--- js/invader.js
+++ js/invader.js
@@ -2,6 +2,18 @@ phina.globalize();
 
 const SCREEN_WIDTH = 960;
 const SCREEN_HEIGHT = 640;
+const ASSETS = {
+    "image" : {
+        "buro" : "./assets/imags/buropiyo.png",
+        "mero" : "./assets/imags/meropiyo.png",
+        "mika" : "./assets/imags/mikapiyo.png",
+        "nasu" : "./assets/imags/nasupiyo.png",
+        "take" : "./assets/imags/takepiyo.png",
+        "toma" : "./assets/imags/tomapiyo.png"
+
+
+    }
+};
 
 phina.define('MainScene',{
     superClass: 'DisplayScene',
@@ -14,6 +26,11 @@ phina.define('MainScene',{
         this.gridY = Grid(SCREEN_HEIGHT,40);
 
         this.backgroundColor = 'black';
+
+        const player = Sprite('toma',64,64).addChildTo(this);
+        player.setFrameIndex(10,64,64)
+        player.x = this.gridX.center();
+        player.y = this.gridY.span(38);
     }
 });
 
@@ -23,6 +40,7 @@ phina.main(() => {
         fps: 60,
         width: SCREEN_WIDTH,
         height:SCREEN_HEIGHT,
+        assets: ASSETS
     });
 
     app.run();
