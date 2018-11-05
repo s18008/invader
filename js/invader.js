4b4704e0dbad76098fe40e5d32dbf21a2603acb8
s18008 <s18008@std.it-college.ac.jp>
2018-11-05 11:25:51 +0900

    playerの移動

--- js/invader.js
+++ js/invader.js
@@ -17,20 +17,45 @@ const ASSETS = {
 
 phina.define('MainScene',{
     superClass: 'DisplayScene',
-    init: function(){
+    init: function() {
         this.superInit({
-            width:SCREEN_WIDTH,
-            height:SCREEN_HEIGHT,
+            width: SCREEN_WIDTH,
+            height: SCREEN_HEIGHT,
         });
-        this.gridX = Grid(SCREEN_WIDTH,40);
-        this.gridY = Grid(SCREEN_HEIGHT,40);
+        this.gridX = Grid(SCREEN_WIDTH, 40);
+        this.gridY = Grid(SCREEN_HEIGHT, 40);
 
         this.backgroundColor = 'black';
 
-        const player = Sprite('toma',64,64).addChildTo(this);
-        player.setFrameIndex(10,64,64)
-        player.x = this.gridX.center();
-        player.y = this.gridY.span(38);
+        const prayer = Player(
+            this.gridX.center(), this.gridY.span(37)).addChild(this);
+    }
+});
+
+phina.define('Player', {
+    superClass: 'Sprite',
+    init: function (x, y) {
+        this.superInit('toma', 64, 64);
+        this.setFrameIndex(10, 64, 64);
+        this.x = x;
+        this.y = y;
+        this.SPPED = 5;
+    },
+
+    update: function (app) {
+        const key = app.keyboard;
+        if (key.getKey('left')) {
+            this.x -= this.SPEED;
+            if (this.left < 0){
+                this.left = 0;
+            }
+        }
+        if (key.getKey('right')) {
+            this.x += this.SPEED;
+            if (this.right > SCREEN_WIDTH){
+                this.right = SCREEN_WIDTH;
+            }
+        }
     }
 });
 
